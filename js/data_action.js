e3fe4a9f4cc75bfd5e344c2f4ee478fbbd38cd07
ercanon <61042236+ercanon@users.noreply.github.com>
2023-11-05 17:29:41 +0100

    Update data_action.js

--- js/data_action.js
+++ js/data_action.js
@@ -136,53 +136,5 @@ data_action = [
         bullets: [
             "When you describe an action not detailed elsewhere in the rules, the DM tells you whether that action is possible and what kind of roll you need to make, if any, to determine success or failure."
         ]
-    },
-    {
-        title: "",
-        icon: "telepathy",
-        subtitle: "Mostly out of combat:"
-    },
-    {
-        title: "",
-        icon: "telepathy",
-    },
-    {
-        title: "",
-        icon: "telepathy",
-    },
-    {
-        title: "Help",
-        icon: "telepathy",
-        subtitle: "Grant an ally advantage",
-        description: "Grant an ally advantage on an ability check or attack",
-        reference: "PHB, pg. 192.",
-        bullets: [
-            "The target gains advantage on the next ability check it makes to perform the task you are helping with.",
-            "Alternatively, the target gains advantage on the next attack roll against against a creature within 5 feet of you.",
-            "The advantage lasts until the start of your next turn."
-        ]
-    },
-    {
-        title: "Search",
-        icon: "magnifying-glass",
-        subtitle: "Search for something",
-        description: "Devote your attention to finding something",
-        reference: "PHB, pg. 193.",
-        bullets: [
-            "Depending on the nature of your search, the DM might have you make a Wisdom (Perception) check or an Intelligence (Investigation) check."
-        ]
-    },
-    {
-        title: "Ready",
-        icon: "stopwatch",
-        subtitle: "Choose trigger and action",
-        description: "Choose a trigger and a response reaction",
-        reference: "PHB, pg. 193.",
-        bullets: [
-            "First, you decide what perceivable circumstance will trigger your reaction.",
-            "Then, you choose the action you will take in response to that trigger, or you choose to move up to your speed in response to it.",
-            "When the trigger occurs, you can either take your reaction right after the trigger finishes or ignore the trigger.",
-            "When you ready a spell, you cast it as normal but hold its energy, which you release with your reaction when the trigger occurs. To be readied, a spell must have a casting time of 1 action, and holding onto the spell's magic requires concentration"
-        ]
     }
 ]
