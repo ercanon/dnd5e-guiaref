180880de966c7f368bffdc359f3f1425ec9f3aa7
ercanon <61042236+ercanon@users.noreply.github.com>
2023-11-06 23:13:13 +0100

    Update data_bonusaction.js

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -8,8 +8,7 @@ data_bonusaction = [
         bullets: [
             "Only usable if you take the <i>Attack</i> action and attack with a light melee weapon that you're holding in one hand.",
             "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
-            "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
-            "If either weapon has the thrown property, you can throw the weapon, instead of making a melee attack with it."
+            "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative."
         ]
     },
     {
