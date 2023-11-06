b9374348eed1e811791b7f996b3a7d6101f5b55d
ercanon <61042236+ercanon@users.noreply.github.com>
2023-11-06 19:33:04 +0100

    Update data_action.js

--- js/data_action.js
+++ js/data_action.js
@@ -8,7 +8,7 @@ data_action = [
         bullets: [
             "Certain features, such as the <i>Extra Attack</i> feature of the fighter, allow you to make more than one attack with this action. Each of these attacks is a separate roll and may target different creatures. You may move in between these attacks.",
             "When you attack with a light melee weapon, you can use a bonus action to attack with your other hand (see the <i>Offhand attack</i> bonus action).",
-            "You may replace one of your melee attacks with a <i>Grapple</i>.",
+            "You may replace one of your melee attacks with a <i>Grapple</i> or <i>NonLethal</i>.",
             "Some conditions give advantage on the attack: attacks against blinded, paralyzed, petrified, restrained, stunned, or unconscious targets; melee attacks against prone targets; attacks by invisible or hidden attackers.",
             "Some conditions give disadvantage on the attack: attacks against invisible or hidden targets; ranged attacks against prone targets; attacks by blinded, frightened, poisoned, or restrained attackers."
         ]
@@ -24,6 +24,17 @@ data_action = [
             "The target of your grapple must be no more than one size larger than you, and it must be within your reach.",
             "Using at least one free hand, you try to seize the target by making a grapple check, a Strength (Athletics) check contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use).",
             "If you succeed, you subject the target to the grappled condition (its speed is set to 0)."
+        ]
+    },
+    {
+        title: "Nonlethal",
+        icon: "sleepy",
+        subtitle: "Special melee attack",
+        description: "Attempt to grab a creature or wrestle with it",
+        reference: "PHB, pg. 195.",
+        bullets: [
+            "You can use the <i>Attack</i> action to make a special melee attack, a nonlethal attack. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
+            "Dropping the target to 0 helth points with a nonlethal attack, you subject it to the unconscious condition."
         ]
     },
     {
