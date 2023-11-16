cfc34ed25801cdfdba012c9a682b5f0a9829c7f4
ercanon <61042236+ercanon@users.noreply.github.com>
2023-11-16 10:37:14 +0100

    Update data_action.js

--- js/data_action.js
+++ js/data_action.js
@@ -71,6 +71,17 @@ data_action = [
         ]
     },
     {
+        title: "Dodge",
+        icon: "suspicious",
+        subtitle: "Increase defenses",
+        description: "Focus entirely on avoiding attacks",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
+            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+        ]
+    },
+    {
         title: "Escape",
         icon: "manacles",
         subtitle: "Escape a grapple",
